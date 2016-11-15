63955feb7c6a6e0d0f10b0ee5acf63ac238fa6eb
maimous@gmail.com <stefaleon@gmail.com>
2016-11-16 01:50:40 +0200

    added express-sanitizer

--- server.js
+++ server.js
@@ -4,19 +4,18 @@ var PORT = process.env.PORT || 3000;
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var methodOverride = require('method-override');
+var expressSanitizer = require('express-sanitizer');
 
 mongoose.connect('mongodb://localhost/restful_blog_app');
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({	extended: true }));
 app.use(methodOverride('_method'));
+app.use(expressSanitizer());
 
 var postSchema = new mongoose.Schema({
 	title: String,
-	image: {
-		type: String,
-		default: 'https://lh3.googleusercontent.com/MgwXHyJOYqAAIHi6dYjKEqPSs_8-ming4v4kg2tM7DityTvSfgtm-s-NEHvQnr5GSQNFKRaCCKocK6Kc8U3GcGl4fEb2DlrxCvV8LJiB7bKAspE5PrqlENGu5m_CnzDeuVNDW_Zwb_8SFRQwK8dK3zbvpgHkX1KXOLdvsS_HOefFix5jsfByghSxxpzgxsRnKWaoRutP5i2BH515SYhC9l6YkxEUCp0Uvz1fO5V0oAWBq5BdVilqYrMxj1232Lu62cmEx4I7LzLSGKeN_EwXbaqoX0WVakjJqob0RfiJvIAegBKjkzsk5R6JTlehUHQA-K5BYgxuT-SuMb-YN1pHTBjlhEkxNlQnlIeCAH33BS_PqNE66_DWCjrSEPOvMtiRn1vUU8Fd_AY_F7y8y3paPfTLmMIzatE8_DmN0-050s7Pfreyp8zLwL_-eLvK60h-p7Cuv-Fcgw7-N_koNaacCQtbKC1Ej2Q1TIKxU11_B-eray2L_L84yAhpMhmtkxBZJCS8ZJ00S7XKoqyR2xn-YHmyeixkx7EDb_keq8grBIoFlCoTrGuYE5lcVi1fjk5CfBJvltHFLfVDn61mLo22bQjUBrc1b6EiueSjklpAPcK-oTt2=w640-h572-no'
-	},
+	image: String,
 	content: String,
 	created: {
 		type: Date,
@@ -26,13 +25,7 @@ var postSchema = new mongoose.Schema({
 
 var Post = mongoose.model('Post', postSchema);
 
-// Post.create({
-// 	title: 'MOTORHEAD',
-// 	image: 'https://i.ytimg.com/vi/vTsa6JTCVaU/maxresdefault.jpg',
-// 	content: "Sunrise, wrong side of another day, Sky high and six thousand miles away, Don't know how long I've been awake, Wound up in an amazing state, Can't get enough, And you know it's righteous stuff, Goes up like prices at Christmas, Motorhead, you can call me Motorhead, alright"
-// });
-
-
+// MAIN
 app.get('/', function(req, res) {
 	res.redirect('posts');
 });
@@ -59,8 +52,8 @@ app.get('/posts/new', function(req, res) {
 app.post('/posts', function(req, res) {
 	var crPost = {
 		title: req.body.title,
-		image: req.body.image || 'https://lh3.googleusercontent.com/MgwXHyJOYqAAIHi6dYjKEqPSs_8-ming4v4kg2tM7DityTvSfgtm-s-NEHvQnr5GSQNFKRaCCKocK6Kc8U3GcGl4fEb2DlrxCvV8LJiB7bKAspE5PrqlENGu5m_CnzDeuVNDW_Zwb_8SFRQwK8dK3zbvpgHkX1KXOLdvsS_HOefFix5jsfByghSxxpzgxsRnKWaoRutP5i2BH515SYhC9l6YkxEUCp0Uvz1fO5V0oAWBq5BdVilqYrMxj1232Lu62cmEx4I7LzLSGKeN_EwXbaqoX0WVakjJqob0RfiJvIAegBKjkzsk5R6JTlehUHQA-K5BYgxuT-SuMb-YN1pHTBjlhEkxNlQnlIeCAH33BS_PqNE66_DWCjrSEPOvMtiRn1vUU8Fd_AY_F7y8y3paPfTLmMIzatE8_DmN0-050s7Pfreyp8zLwL_-eLvK60h-p7Cuv-Fcgw7-N_koNaacCQtbKC1Ej2Q1TIKxU11_B-eray2L_L84yAhpMhmtkxBZJCS8ZJ00S7XKoqyR2xn-YHmyeixkx7EDb_keq8grBIoFlCoTrGuYE5lcVi1fjk5CfBJvltHFLfVDn61mLo22bQjUBrc1b6EiueSjklpAPcK-oTt2=w640-h572-no',
-		content: req.body.content
+		image: req.body.image, 
+		content: req.sanitize(req.body.content)
 	}
 	Post.create(crPost, function(err, newPost) {
 		if (err) {
@@ -102,7 +95,7 @@ app.put('/posts/:id', function(req, res) {
 	var updPost = {
 		title: req.body.title,
 		image: req.body.image,
-		content: req.body.content,
+		content: req.sanitize(req.body.content),
 		updated: Date.now
 	}
 	Post.findByIdAndUpdate(req.params.id, updPost, function(err, updatedPost) {
